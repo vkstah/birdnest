25ee4554cdc1f291636a2ff5f8e9830a66cda003
Vili Ståhlberg <vili@sivulabra.fi>
2023-01-03 12:01:40 +0200

    Remove unused parameter from load function

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,6 +1,6 @@
 import type { PageLoad } from "./$types";
 
-export const load = (async ({ fetch, depends }) => {
+export const load = (async ({ fetch }) => {
 	const response = await fetch(`/api/drones`);
 	return await response.json();
 }) satisfies PageLoad;
