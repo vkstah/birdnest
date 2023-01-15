d1958253631387eb5624cdef8f07b1bee9ec6fb4
Vili Ståhlberg <vili@sivulabra.fi>
2023-01-15 12:22:14 +0200

    Update Tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["./src/**/*.{html,js,svelte,ts}"],
-	theme: {
-		fontFamily: {
-			body: ["Inter", "sans-serif"]
-		},
-		extend: {}
-	},
-	plugins: []
+  content: ["./src/**/*.{html,js,svelte,ts}"],
+  theme: {
+    extend: {}
+  },
+  plugins: []
 };
