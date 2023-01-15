46766d83353e8b88464488b82736df67f6515185
Vili Ståhlberg <vili@sivulabra.fi>
2023-01-15 12:20:49 +0200

    Add JSDoc comments for better code readability

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -18,7 +18,17 @@ export const isDroneViolating = (drone: Drone) => {
   }
 };
 
+/**
+ * Adds a leading zero. Useful for displaying time. For example: 9:15 vs. 09:15.
+ * @param num The number.
+ * @returns The number as string including the zero (if applicable)
+ */
 const leadingZero = (num: number) => `0${num}`.slice(-2);
 
+/**
+ * Formats a date object into a time string HH:MM:SS
+ * @param date Date object.
+ * @returns Time string.
+ */
 export const formatTime = (date: Date): string =>
   [date.getHours(), date.getMinutes(), date.getSeconds()].map(leadingZero).join(":");
